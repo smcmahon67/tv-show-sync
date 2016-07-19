08b68dde86cbde8d959f10cc3e6c1bf4dd464d19
smcmahon67 <steve@mcmahonfamily.id.au>
2016-07-19 22:00:08 +1000

    added some show names so that the names will be consistent.

---
+++ getepg-freeepg.ps1
@@ -0,0 +1,3 @@
+﻿# script to grab the latest XML EPG from freepg.tv
+Set-Location -Path "C:\ProgramData\ARGUS TV\XMLTV"
+Invoke-WebRequest -Uri "http://au.freepg.tv/xmltv/oztivo/ACT.Canberra.gz?UID=DD81BE72&K=C9709EEB10218DF59044F422B21EBD04" -OutFile "epg.xml"
